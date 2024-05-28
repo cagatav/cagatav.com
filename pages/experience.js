0b7dee660a5edef1887acaa3fb04e5777d922c3e
cagatav <cgty.szn@gmail.com>
2024-05-28 15:26:21 +0300

    hot fix about timeline

--- pages/experience.js
+++ pages/experience.js
@@ -1,46 +1,46 @@
 import Timeline from '@/pages/timeline';
-import Head from 'next/head'
+import Head from 'next/head';
 
 const experiences = [
-    {
-      title: "Software Developer Intern, PopupSmart",
-      date: "September 2023 - Present",
-      description: [
-        "Working on the LiveChatAI product.",
-        "Engaged in web and artificial intelligence projects.",
-        "Gained experience with TailwindCSS, ReactJS, and NextJS.",
-        "Developed skills in testing and bug detection.",
-        "Improved my formal email writing skills and customer relations in English while providing technical support.",
-        "Acquired experience in product management."
-      ]
-    },
-    {
-        title: "Mobile Application Developer Intern, Beytepe Bilisim",
-        date: "April 2023 - August 2023 (5 Month)",
-        description: [
-          "Worked as a long-term intern.",
-          "Redeveloped an existing hotel management application using Java in Android Studio."
-        ]
-    },
-    {
-        title: "Intern, Uluman Technology",
-        date: "August 2022 - September 2022 (1 Month)",
-        description: [
-          "Developed my first mobile application using Java in Android Studio.",
-          "Learned to establish internal company interactions and teamwork.",
-          "The developed application is available on GitHub."
-        ]
-    },
-  ];
+  {
+    title: "Software Developer Intern, PopupSmart",
+    date: "September 2023 - Present",
+    description: [
+      "Working on the LiveChatAI product.",
+      "Engaged in web and artificial intelligence projects.",
+      "Gained experience with TailwindCSS, ReactJS, and NextJS.",
+      "Developed skills in testing and bug detection.",
+      "Improved my formal email writing skills and customer relations in English while providing technical support.",
+      "Acquired experience in product management."
+    ]
+  },
+  {
+    title: "Mobile Application Developer Intern, Beytepe Bilisim",
+    date: "April 2023 - August 2023 (5 Month)",
+    description: [
+      "Worked as a long-term intern.",
+      "Redeveloped an existing hotel management application using Java in Android Studio."
+    ]
+  },
+  {
+    title: "Intern, Uluman Technology",
+    date: "August 2022 - September 2022 (1 Month)",
+    description: [
+      "Developed my first mobile application using Java in Android Studio.",
+      "Learned to establish internal company interactions and teamwork.",
+      "The developed application is available on GitHub."
+    ]
+  }
+];
 
 export default function Experience() {
   return (
     <div className="container mx-auto p-4">
-    <Head>
+      <Head>
         <title>C A G A T A V - Experience</title>
         <meta name="description" content="CAGATAV" />
         <link rel="icon" href="/icon-white.svg" />
-    </Head>
+      </Head>
       <h1 className="text-3xl font-semibold text-center mb-8 text-[#903b3b]">My Experiences</h1>
       <Timeline experiences={experiences} />
     </div>
--- pages/timeline.js
+++ pages/timeline.js
@@ -1,23 +1,24 @@
-const Timeline = ({ experiences }) => {
-    return (
-      <div className="timeline">
-        {experiences.map((experience, index) => (
-          <div key={index} className="timeline-item">
-            <div className="timeline-dot"></div>
-            <div className="timeline-content">
-              <h3 className="timeline-title">{experience.title}</h3>
-              <p className="timeline-date">{experience.date}</p>
+const Timeline = ({ experiences = [] }) => {
+  return (
+    <div className="timeline">
+      {experiences.map((experience, index) => (
+        <div key={index} className="timeline-item">
+          <div className="timeline-dot"></div>
+          <div className="timeline-content">
+            <h3 className="timeline-title">{experience.title}</h3>
+            <p className="timeline-date">{experience.date}</p>
+            {experience.description && (
               <ul className="list-disc list-inside">
                 {experience.description.map((item, idx) => (
                   <li key={idx}>{item}</li>
                 ))}
               </ul>
-            </div>
+            )}
           </div>
-        ))}
-      </div>
-    );
-  };
-  
-  export default Timeline;
-  
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Timeline;
