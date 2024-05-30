62da9c7c770761213b5b1f522dbfa1a961749d2a
cagatav <112089960+cagatav@users.noreply.github.com>
2024-05-30 19:59:43 +0300

    cv download link update

--- pages/cv.js
+++ pages/cv.js
@@ -11,7 +11,7 @@ export default function CV() {
       </Head>
       <h1 className="text-3xl font-bold mb-10 text-center text-[#903b3b]">My CV</h1>
       <div className="mb-4 text-center">
-        <Link href="https://s6.dosya.tc/en2.php?a=server20/pwac0c/CV.pdf&b=dfed725b982afbffa904f3d173be10f8&c=1716986499">
+        <Link href="https://s2.dosya.tc/en2.php?a=server30/uqq5kr/EnesCagataySOZEN-CV.pdf&b=d7511eca727e9e38efa2fa1ae5bef85c&c=1717088300">
           <span className="bg-[#903b3b] text-white py-3 px-4 rounded hover:bg-[#6c2c2c] text-sm" target="_blank">Download CV (PDF)</span>
         </Link>
       </div>
