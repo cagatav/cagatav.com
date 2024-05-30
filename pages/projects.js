c88f8632d0ee9ddf1ee270eb93a58c30e5590502
cagatav <112089960+cagatav@users.noreply.github.com>
2024-05-31 00:18:34 +0300

    title fix

--- pages/projects.js
+++ pages/projects.js
@@ -1,12 +1,19 @@
 import projects from '../data/projectsdata';
 import ProjectCard from '../components/projectscard';
 import { useTheme } from '../components/ThemeContext';
+import Head from 'next/head';
+
 
 export default function Projects() {
   const { darkMode } = useTheme();
 
   return (
     <div className={` min-h-screen min-w-fit px-30 container mx-auto p-4 transition-all duration-300 ${darkMode ? 'bg-gray-900 text-white' : 'bg-gray-100 text-black'}`}>
+      <Head>
+        <title>C A G A T A V - CV</title>
+        <meta name="description" content="CAGATAV" />
+        <link rel="icon" href="/icon-white.svg" />
+      </Head>
       <h1 className="text-3xl font-bold mb-4 text-[#903b3b] text-center">Projects</h1>
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-6">
         {projects.map(project => (
