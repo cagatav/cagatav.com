16d3760f978c7c158a806aa0da4a807036041fc3
cagatav <cgty.szn@gmail.com>
2024-09-09 14:27:18 +0300

    test

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,8 @@ export default function index() {
       <Head>
         <title>C A G A T A V</title>
         <link rel="icon" href="/icon-white.svg" />
-        <script
-          src="https://app.livechatai.com/embed.js"
-          data-id="clxu16c2u000jllbl60jn2rmd"
-          async defer>
-        </script>
+        
+
       </Head>
     <Home/>
     </div>
