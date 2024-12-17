48a3cc1d4de49771d31400e3041a4b464013c530
cagatav <cgty.szn@gmail.com>
2024-12-17 12:01:36 +0300

    test

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
-          src="https://live-chat-ai-web-git-feature-sc-14728adding-i-b3c896-popupsmart.vercel.app/embed.js"
-          data-id="cm4miw4hw0001mh0aeubmt57h"
-          async defer>
-        </script>
       </Head>
       <body>
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,6 @@ export default function index() {
       <Head>
         <title>C A G A T A V</title>
         <link rel="icon" href="/icon-white.svg" />
-        <script
-          src="https://live-chat-ai-web-git-feature-sc-14728adding-i-b3c896-popupsmart.vercel.app/embed.js"
-          data-id="cm4miw4hw0001mh0aeubmt57h"
-          async defer>
-        </script>
       </Head>
     <Home/>
     </div>
