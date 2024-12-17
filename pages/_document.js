7a1caa36080923ce99799623012d12574e8c96e3
cagatav <cgty.szn@gmail.com>
2024-12-17 13:29:30 +0300

    test

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <script
+        src="https://app-dev.livechatai.com/embed.js"
+        data-id="cm4sbdyb00001jo0brcxqgtqy"
+        async defer>
+      </script>
       </Head>
       <body>
         <Main />
