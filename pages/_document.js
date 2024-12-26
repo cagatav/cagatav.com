d77b7bd330a7496d5c56a744a7217350ac1ea0e0
cagatav <cgty.szn@gmail.com>
2024-12-26 13:05:22 +0300

    lca test

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <script
+    src="https://live-chat-ai-web-git-feature-sc-14743directin-9e6daf-popupsmart.vercel.app/embed.js"
+    data-id="cm4sbdyb00001jo0brcxqgtqy"
+    async defer>
+  </script>
       </Head>
       <body>
         <Main />
